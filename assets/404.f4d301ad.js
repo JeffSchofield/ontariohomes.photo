ac6a70f1955d47dfbb345713f897c05fcc2a576e
Jeff Schofield <jeff@jeffschofield.com>
2022-07-24 23:12:53 -0400

    fix: Support getting base from Vite for use in vue router

--- assets/404.f4d301ad.js
+++
@@ -1,1 +0,0 @@
-import{a as n,c as a}from"./index.8d6d30ad.js";var _=(e,r)=>{const t=e.__vccOpts||e;for(const[c,o]of r)t[c]=o;return t};const s={};function f(e,r){return n(),a("div",null,"404 Not Found")}var u=_(s,[["render",f]]);export{u as default};
